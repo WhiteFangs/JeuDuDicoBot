65a1601282fb1b000585792d7b53b4c87f3606b7
WhiteFangs <white-fangs@hotmail.fr>
2017-05-20 18:54:17 +0200

    Initial commit

---
+++ JeuDuDicoBot.php
@@ -0,0 +1,176 @@
+<?php
+
+include('dbinfo.php');
+include('twitterCredentials.php');
+require_once('TwitterAPIExchange.php');
+header('Content-Type: text/html; charset=utf-8');
+
+// Set Twitter official app's access tokens, see README.md
+$APIsettingsAndroid = array(
+    'oauth_access_token' => $oauthTokenAndroid,
+    'oauth_access_token_secret' => $oauthTokenSecretAndroid,
+    'consumer_key' => $consumerKeyAndroid,
+    'consumer_secret' => $consumerSecretAndroid
+);
+
+/** Set access tokens here - see: https://apps.twitter.com/ **/
+$APIsettings = array(
+    'oauth_access_token' => $oauthToken,
+    'oauth_access_token_secret' => $oauthTokenSecret,
+    'consumer_key' => $consumerKey,
+    'consumer_secret' => $consumerSecret
+);
+
+$twitterAndroid = new TwitterAPIExchange($APIsettingsAndroid);
+$twitter = new TwitterAPIExchange($APIsettings);
+
+// Connect to applications DB
+try {
+    $bddApp = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbAppName, $dbAppLogin, $dbAppPassword, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    $bddApp->exec("SET CHARACTER SET utf8");
+} catch (PDOException $e) {
+    echo 'Echec de la connexion : ' . $e->getMessage();
+    exit;
+}
+
+// Get previous poll's tweet id and right answer
+$appResponse = $bddApp->query("SELECT value FROM keyvalue WHERE app='jeuDuDicoBotTweetId'");
+$appResult = $appResponse->fetchAll();
+$tweetid = $appResult[0][0];
+$appResponse = $bddApp->query("SELECT value FROM keyvalue WHERE app='jeuDuDicoBotWord'");
+$appResult = $appResponse->fetchAll();
+$word = $appResult[0][0];
+$appResponse->closeCursor();
+
+// If poll's tweet exists
+if(strlen($tweetid) > 0 && strlen($word) > 0){
+    $tweet = "Le terme correspondant à cette définition était... " . $word . " ! Bien joué à tous ! Et tout de suite, une nouvelle énigme...";
+    // Post the tweet with the right word in reply to the previous poll's tweet
+    $postfields = array(
+        'status' =>  $tweet,
+        'in_reply_to_status_id' => $tweetid
+        );
+    $url = "https://api.twitter.com/1.1/statuses/update.json";
+    $requestMethod = "POST";
+
+    echo $twitter->buildOauth($url, $requestMethod)
+                  ->setPostfields($postfields)
+                  ->performRequest();
+}
+
+// Connect to rare words DB
+try {
+    $bdd = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $dblogin, $dbpassword, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    $bdd->exec("SET CHARACTER SET utf8");
+} catch (PDOException $e) {
+    echo 'Echec de la connexion : ' . $e->getMessage();
+    exit;
+}
+
+// Get a rare word and at least another one with close lexical properties
+$result = array();
+do{
+    $queryWord = 'SELECT * FROM rares WHERE lexinfo IS NOT NULL ORDER BY RAND() LIMIT 1';
+    $response = $bdd->query($queryWord);
+    $result = $response->fetchAll();
+    $wordObject = $result[0];
+    $response->closeCursor();
+
+    $lexinfo = $wordObject['lexinfo'];
+
+    $lexQuery = "";
+    $lexparts = explode(".", $lexinfo);
+    foreach ($lexparts as $key => $part) {
+        if(strpos($part, "et ") !== FALSE)
+            $part = str_replace("et ", "", $part);
+        $part = trim($part);
+        if(strlen($part) == 0)
+            continue;
+        if($key != 0)
+            $lexQuery .= "AND ";
+        $lexQuery .= 'lexinfo LIKE "%' .$part . '.%" ';
+    }
+
+    $queryWord = 'SELECT word FROM rares WHERE '.$lexQuery.' AND word!="'.$wordObject['word'].'" ORDER BY RAND() LIMIT 3';
+    $response = $bdd->query($queryWord);
+    $result = $response->fetchAll();
+    $response->closeCursor();
+}while(count($result) == 0);
+
+// Add all words in one array and shuffle
+$def = $wordObject['def'];
+$words = array($wordObject['word']);
+foreach ($result as $word) {
+    $words[] = $word[0];
+}
+shuffle($words);
+
+var_dump($words);
+
+// Create poll card with words
+$cards = array(
+    'twitter:long:duration_minutes' => 1440,
+    'twitter:api:api:endpoint' => '1',
+    'twitter:card' => 'poll'. count($words) .'choice_text_only',
+    );
+foreach ($words as $key => $word) {
+    $cards['twitter:string:choice' . ($key+1) . '_label'] = $word;
+}
+
+var_dump($cards);
+
+// Post the poll card
+$postfields = array(
+    'card_data' =>  json_encode($cards),
+    'send_error_codes' => 1
+);
+$url = "https://caps.twitter.com/v2/cards/create.json";
+$requestMethod = "POST";
+// Use the user-agent of the official app
+$curlOptions = array(CURLOPT_USERAGENT => 'TwitterAndroid/6.45.0 Nexus 4/17 (LGE;mako;google;occam;0)');
+$card_data = $twitterAndroid->buildOauth($url, $requestMethod)
+              ->setPostfields($postfields)
+              ->performRequest(true, $curlOptions);
+
+$card_data = json_decode($card_data);
+
+var_dump($card_data);
+
+// If card has been created
+if(isset($card_data->card_uri)){
+    $tweet = $def;
+    if(strlen($tweet) > 140)
+      $tweet = substr($tweet, 0, 140 - 3) . "...";
+
+    // Post the tweet with the poll card and the definition
+    $postfields = array(
+        'status' =>  $tweet,
+        'card_uri' => $card_data->card_uri,
+        'cards_platform' => 'Android-10',
+        'include_cards' => 1
+        );
+    $url = "https://api.twitter.com/1.1/statuses/update.json";
+    $requestMethod = "POST";
+    $tweet_data = $twitter->resetFields()->buildOauth($url, $requestMethod)
+                  ->setPostfields($postfields)
+                  ->performRequest();
+
+    $tweet_data = json_decode($tweet_data);
+
+    var_dump($tweet_data);
+	
+	// If successful, store the tweet's id and the right word answer in the application DB, else don't reply to inexisting poll next time
+    if(isset($tweet_data->id_str)){
+        $appResponse = $bddApp->query("UPDATE keyvalue SET value = '". $tweet_data->id_str ."' WHERE app = 'jeuDuDicoBotTweetId'");
+        $appResponse = $bddApp->query("UPDATE keyvalue SET value = '". $wordObject['word'] ."' WHERE app = 'jeuDuDicoBotWord'");
+    }else{
+        $appResponse = $bddApp->query("UPDATE keyvalue SET value = '' WHERE app = 'jeuDuDicoBotTweetId'");
+        $appResponse = $bddApp->query("UPDATE keyvalue SET value = '' WHERE app = 'jeuDuDicoBotWord'");
+    }
+}else{
+    $appResponse = $bddApp->query("UPDATE keyvalue SET value = '' WHERE app = 'jeuDuDicoBotTweetId'");
+    $appResponse = $bddApp->query("UPDATE keyvalue SET value = '' WHERE app = 'jeuDuDicoBotWord'");
+}
+$appResponse->closeCursor();
+
+?>
---
+++ README.md
@@ -0,0 +1,41 @@
+# Jeu du Dico Bot
+
+A Twitter bot that tweets a definition of a rare french word and a poll with several words, one being the actual definition's word. 
+
+The Twitter bot is [@JeuDuDicoBot](https://twitter.com/JeuDuDicoBot)
+
+## How it works
+The program looks for a word in the rare words database and for three other words with close lexical properties.
+The tweet is the definition of the first word, the poll choices are made of all the words shuffled.
+The program then posts the poll and the tweet containing the poll, and stores the tweet's id and the right word for the poll in a database.
+Next time the program is run, it will begin by tweeting the right answer for the poll in reply to the poll's tweet.
+
+## Post a Twitter poll
+Posting a Twitter poll is currently not supported in the official Twitter Rest API, so you'll need to hack your way to post one. For that, you must have OAuth Access Tokens for an official Twitter application that supports polls, for instance Twitter for iPhone or Twitter for Android. 
+In this program I used Android, but [@fourtonfish](https://github.com/fourtonfish) has made a [good example for iPhone in Python](https://gist.github.com/fourtonfish/5ac885e5e13e6ca33dca9f8c2ef1c46e).
+
+  1. Create your Twitter bot's account
+  2. Use [Twitter's PIN-based authorization](https://dev.twitter.com/oauth/pin-based) with the [chosen app's consumer keys](https://gist.github.com/shobotch/5160017) and your bot's account to get your official app's OAuth Tokens
+  3. Create a [Twitter application](https://apps.twitter.com) for your bot and get your app's OAuth Tokens
+  4. Create the card_data parameter of your call that looks like this:
+  ```card = {
+	'twitter:string:choice1_label': pages[0],
+	'twitter:string:choice2_label': pages[1],
+	'twitter:string:choice3_label': pages[2],
+	'twitter:string:choice4_label': pages[3],
+	'twitter:long:duration_minutes': 1440,
+	'twitter:api:api:endpoint': '1',
+	'twitter:card': 'poll4choice_text_only', // poll2choice_text_only if you only have 2 choices
+}```
+  5. Don't forget to stringify (or JSON encode) the card object.
+  6. When you make the call to `https://caps.twitter.com/v2/cards/create.json`, use the user-agent of the official app you chose (in my example: `TwitterAndroid/6.45.0 Nexus 4/17 (LGE;mako;google;occam;0)`)
+
+
+## Databases
+It uses two databases: 
+  - a database of rare french words with their definitions and lexical informations (part of speech, gender and number)
+  - a database to store the id of a tweet containing the current poll and the right word for this poll
+
+## License
+The source code of this bot is available under the terms of the [MIT license](http://www.opensource.org/licenses/mit-license.php).
+
